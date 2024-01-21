fcfbea7ea00fa18eada92dd404f6c8bec9fd4acc
Van Hung <vanhung240403@gmail.com>
2024-01-21 22:25:20 +0700

    dmm

--- React-js-Master-Detail-CRUD-with-Asp.Net-Web-API/restaurant/src/api/index.js
+++ React-js-Master-Detail-CRUD-with-Asp.Net-Web-API/restaurant/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://35.240.134.185::5000/api/';
+const BASE_URL = 'http://35.240.134.185:5000/api/';
 
 export const ENDPIONTS = {
     CUSTOMER: 'Customer',
